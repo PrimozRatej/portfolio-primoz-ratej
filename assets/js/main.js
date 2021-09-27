629db05136e8eebae00ccf1cbdbaab7e43487441
primozratej@gmail.com <primozratej@gmail.com>
2021-09-27 08:16:45 +0200

    Prod ver0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
     // Typed Initiate
     if ($('#typed-text-show').length == 1) {
-        var typed_strings = "Web Designer, Web Developer, Front End Developer, Apps Designer, Apps Developer";
+        var typed_strings = "Web Developer, Front End Developer, Apps Developer, Back End Developer, Automation Tester";
         var typed = new Typed('#typed-text-show', {
             strings: typed_strings.split(', '),
             typeSpeed: 100,
