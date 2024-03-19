5cb3458606c6d6b9d6204d602d43afdcb1bd800f
primozratej <primozratej@gmail.com>
2024-03-19 12:29:34 +0700

    Update resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 (function ($) {
-    // Typed Initiate
-    if ($('#typed-text-show').length == 1) {
-        var typed_strings = "Web Developer, Front End Developer, Apps Developer, Back End Developer, Automation Tester";
-        var typed = new Typed('#typed-text-show', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
-    }
+  // Typed Initiate
+  if ($("#typed-text-show").length == 1) {
+    var typed_strings =
+      "Web Developer, Front-end Developer, Mobile Developer, Back-end Developer, Automation Tester.";
+    var typed = new Typed("#typed-text-show", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
+    });
+  }
 })(jQuery);
-
